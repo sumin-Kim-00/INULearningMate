ceb06beffa88174cf74d26b59d09cbee7f9b2af3
Day <cyandl1103@naver.com>
2022-02-21 01:20:47 +0900

    form 태그 사용해서 post 방식 request 처리 연습... 채팅용으로 html 하나만 쓰던데 이 방식 어떻게 활용해보면 될듯..?

--- myapp/views.py
+++ myapp/views.py
@@ -5,8 +5,19 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     context = {}
 
-    return render(request, "chathome.html", context)
+    return render(request, "test.html", context)
 
+def result(request):
+    context = {}
+    name = request.POST['username']
+    students = ['amy', 'bob', 'catherine', 'dennis', 'ethan']
+
+    if name in students:
+        is_exist = True
+    else:
+        is_exist = False
+
+    return render(request, 'test.html', {'user_name': name, 'is_exist': is_exist})
 
 
 """
