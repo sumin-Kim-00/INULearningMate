233e4028ca7608365801bde61284a39ce10464f2
Day <cyandl1103@naver.com>
2022-02-21 01:20:47 +0900

    form 태그 사용해서 post 방식 request 처리 연습... 채팅용으로 html 하나만 쓰던데 이 방식 어떻게 활용해보면 될듯..?

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('result', views.result, name='result')
     #path('create/', views.create),
     #path('read/<id>/', views.read)
 ]
