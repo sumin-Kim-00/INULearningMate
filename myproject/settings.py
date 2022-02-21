55180561ba40f0abd9f3b0d421226426b07cb875
Day <cyandl1103@naver.com>
2022-02-21 13:21:30 +0900

    외부에서 접속 가능하게 settings 수정

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-67&u(=x@%f5-*p$e%%-&)j(-_qxi(9wc&w_8cz*fud%e!ugl%7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
