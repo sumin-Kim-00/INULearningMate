c4bdc2b7c6037cf3fcb7a81166cf3a73be6c409c
null <78254198+sumin-Kim-00@users.noreply.github.com>
2022-02-24 02:21:31 +0900

    첫 메세지랑 강의명 알려쥼

--- myapp/views.py
+++ myapp/views.py
@@ -84,7 +84,6 @@ def login(request):
         print(names)
         print()
         context['courses'] = names
-        print(context)
         return render(request, 'chathome.html', context)
 
     # 로그인 실패시 알림 띄우고 다시 로그인 창으로 돌아감
