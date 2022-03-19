4446e31a141b7401581033aebe74d84e24003713
Day <cyandl1103@naver.com>
2022-03-19 15:40:52 +0900

    bug fix and enhanced selenium performance

--- myapp/views.py
+++ myapp/views.py
@@ -35,6 +35,17 @@ year = '2022'
 semester = '1'
 
 options = webdriver.ChromeOptions()
+
+prefs = {'profile.default_content_setting_values': {
+    'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 
+    'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 
+    'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 
+    'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 
+    'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 
+    'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 
+    'site_engagement' : 2, 'durable_storage' : 2}} 
+options.add_experimental_option('prefs', prefs)
+
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
