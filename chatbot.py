832ad136afdb5049f1ccf822d1b09f19958bf97f
umyujeong <dano006@naver.com>
2022-03-21 12:42:52 +0900

    ohta

--- chatbot.py
+++ chatbot.py
@@ -93,25 +93,22 @@ def detect_intent_texts(crawler, project_id, session_id, texts, language_code, n
                 
         
         if  "강의" in ftext:
-            if "이번주" in ftext:
-                print("df")
+            if in_names:
+                print("답장 :", ftext)
+                table = crawler.course_check(course_name,course_name_id)
+                crawler.not_checked(table)
+                result = ""
+                result += '<'+ course_name +'><br><br>'
+                result += crawler.print_table(table, 'c')
+                print(result)
+                return result
             else:
-                if in_names:
-                    print("답장 :", ftext)
-                    table = crawler.course_check(course_name,course_name_id)
-                    crawler.not_checked(table)
-                    result = ""
-                    result += '<'+ course_name +'><br><br>'
-                    result += crawler.print_table(table, 'c')
-                    print(result)
-                    return result
-                else:
-                    result="어떤 과목의 강의 출석 현황을 알고 싶으세요?<br>"
-                    result += '<div class="cnBtn">'
-                    for i in range(len(names)):
-                        result += '<div><button onclick="cn_btn_course()" value="'+ names[i] + '">'+ names[i]+'</button></div>'
-                    result += '</div>'
-                    return result
+                result="어떤 과목의 강의 출석 현황을 알고 싶으세요?<br>"
+                result += '<div class="cnBtn">'
+                for i in range(len(names)):
+                    result += '<div><button onclick="cn_btn_course()" value="'+ names[i] + '">'+ names[i]+'</button></div>'
+                result += '</div>'
+                return result
 
             
         if "과제" in ftext:
